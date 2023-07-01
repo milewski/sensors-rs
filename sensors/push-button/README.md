52cf1361f1c753dbe38fefac747494653f550377
Rafael Milewski <rafael.milewski@gmail.com>
2023-07-01 18:08:42 +0800

    add push-button example

---
+++ sensors/push-button/README.md
@@ -0,0 +1,19 @@
+# Push Button Example
+
+This example switch on/off a let when the button is pressed.
+
+### Run
+
+Run the project via:
+
+```bash
+cargo flash -p push-button --release --chip STM32F103C8
+```
+
+https://github.com/milewski/sensors-rs/assets/2874967/7b9bd746-2766-4ed1-a2ea-0e183278108c
+
+<details>
+  <summary>Device</summary>
+
+![push-button](https://github.com/milewski/sensors-rs/assets/2874967/b6ccd233-3915-4ae8-bf93-7803a7ec74b6)
+</details>
---
+++ sensors/push-button/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![allow(unused_imports)]
+
+use cortex_m_rt::entry;
+use panic_halt;
+use stm32f1xx_hal::{pac::Peripherals as DevicePeripherals, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let device_peripheral = DevicePeripherals::take().unwrap();
+
+    let mut gpioa = device_peripheral.GPIOA.split();
+    let mut gpiob = device_peripheral.GPIOB.split();
+
+    let button = gpioa.pa10.into_pull_up_input(&mut gpioa.crh);
+    let mut led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+
+    loop {
+        if button.is_high() {
+            led.set_low();
+        } else {
+            led.set_high();
+        }
+    }
+}
